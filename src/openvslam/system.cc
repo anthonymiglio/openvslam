4c10ebea22d49dd25bbf1153cb7cd0f4d960270f
Anthony Pinto <66563366+anthonymiglio@users.noreply.github.com>
2021-01-10 15:40:47 -0300

    Getting Landmarks

--- src/openvslam/system.cc
+++ src/openvslam/system.cc
@@ -11,6 +11,8 @@
 #include "openvslam/io/map_database_io.h"
 #include "openvslam/publish/map_publisher.h"
 #include "openvslam/publish/frame_publisher.h"
+#include "openvslam/data/landmark.h"
+#include "openvslam/data/keyframe.h"
 
 #include <thread>
 
